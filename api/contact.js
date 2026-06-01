ef6942bc644111b150abab2800890ab83260b153
GroveX-DK <ebestuurder@gmail.com>
2026-06-01 21:21:02 +0200

    Update contact.js

--- api/contact.js
+++ api/contact.js
@@ -19,7 +19,7 @@ async function getClient() {
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 465,
-  secure: false,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
